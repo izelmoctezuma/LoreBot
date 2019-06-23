71f4bffc7fd440f6af05562d76a9be64eeb15bdd
izelmoctezuma <paolamoctezuma@STING.fios-router.home>
2019-06-23 11:16:16 -0400

    initial commit

---
+++ DM-bot.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import discord
+import gspread
+import pandas as pd
+from oauth2client.service_account import ServiceAccountCredentials
+
+
+BOT_TOKEN = "NTkyMTkyNDU5NTc2OTY3MTY4.XQ7xYg.LM4irLYwS6oNdLLeyhgIx6Zyw74"
+
+client = discord.Client()   
+
+def get_data(name, attribute):
+    # Google Sheet access
+    scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
+    google_key = '/Users/paolamoctezuma/PythonDev/DMBot/api-keys.json'
+    creds = ServiceAccountCredentials.from_json_keyfile_name(google_key, scope)
+    client = gspread.authorize(creds)
+    sheet = client.open('DMBot Database')
+    wsheet = sheet.worksheet('Characters')
+    
+    # Extract all the values - this returns a dictionary
+    sheet1_data = wsheet.get_all_records()
+    data = pd.DataFrame(sheet1_data)
+    data.columns = data.columns.str.lower()
+    return data[data['name'].str.lower()==name.lower()][attribute.lower()][0]
+
+@client.event
+async def on_message(message):
+    # we do not want the bot to reply to itself
+    if message.author == client.user:
+        return
+    if message.content.startswith('!hello'):
+        msg = 'Hello, {0.author.mention}!'.format(message)
+        await message.channel.send(msg)
+    if message.content.startswith('!say'):
+        arg_pos = message.content.find(' ')
+        msg = 'Okay, I\'m saying ' + message.content[arg_pos + 1:]
+        await message.channel.send(msg)
+    if message.content.startswith('!lookup'):
+            args = message.content.split()
+            msg = get_data(args[1], args[2])
+            await message.channel.send(args[1]+ '\'s ' + args[2] + ' is: ' + msg)
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+# Start the BOT!
+client.run(BOT_TOKEN)
+
